2c1f8b8d95b6fee21abde5087a6356cccccf3ab2
syed mohammed <85601222+syedmohammedgouse@users.noreply.github.com>
2021-06-09 22:42:39 +0530

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,18 @@
+# import the module
+import python_weather
+import asyncio
+
+async def getweather():
+    # declare the client. format defaults to metric system (celcius, km/h, etc.)
+    client = python_weather.Client(format=python_weather.IMPERIAL)
+
+    # fetch a weather forecast from a city
+    # please change the city name at washington dc
+    weather = await client.find("Washington DC")
+
+    # close the wrapper once done
+    await client.close()
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(getweather())
